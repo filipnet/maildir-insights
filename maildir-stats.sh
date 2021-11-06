3c1cae209a2f786b9d93da1f230e320e67642e5e
filipnet <63908077+filipnet@users.noreply.github.com>
2021-11-06 07:39:32 +0100

    Create maildir-stats.sh

---
+++ maildir-stats.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Script Name   : maildir-stats.sh
+# Description   : Useful statistical output of Maildir user mailboxes
+# Author        : https://github.com/filipnet/maildir-stats
+# License       : BSD 3-Clause "New" or "Revised" License
+# =======================================================================================
+
+renice -n 10 $$ > /dev/null
+export LC_ALL=de_DE.utf8
+
+CURRENTDATE=`date '+%Y-%m-%d'`
+TIME_START=$(date +"%s")
+HOMEDIR="/home"
+USERDIR=(`find $HOMEDIR -mindepth 1 -maxdepth 1 -type d`)
+
+TABLE="HOMEDIR|SIZE|COUNT|DIR|READ|UNREAD|TRASH\n"
+for i in "${USERDIR[@]}"
+do
+	:
+	MAILBOX_MAILDIR="$i/Maildir"
+	MAILBOX_SIZE=`du $i/Maildir -sh | cut -f1`
+	MAILBOX_COUNT=`find $i/Maildir -type f | wc -l`
+	MAILBOX_DCOUNT=`find $i/Maildir -type d | wc -l`
+	MAILBOX_READ=`find $i/Maildir -type f | grep -E ',[^,]*S[^,]*$' | wc -l`
+	MAILBOX_UNREAD=`find $i/Maildir -type f | grep -vE ',[^,]*S[^,]*$' | wc -l`
+	MAILBOX_TRASH=`find $i/Maildir/.Trash -type f 2> /dev/null | wc -l`
+
+	TABLE+="${MAILBOX_MAILDIR}|${MAILBOX_SIZE}|${MAILBOX_COUNT}|${MAILBOX_DCOUNT}|${MAILBOX_READ}|${MAILBOX_UNREAD}|${MAILBOX_TRASH}\n"
+done
+
+TIME_DIFF=$(($(date +"%s")-${TIME_START}))
+#echo -e $TABLE
+echo -e $TABLE | column -t -s "|"
+echo -e "\nRuntime: $((${TIME_DIFF} / 60)) Minutes $((${TIME_DIFF} % 60)) Seconds\n"
